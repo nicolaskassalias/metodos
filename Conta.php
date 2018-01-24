a7b0c6e7e5e1754cf5c68c22c812bb794498573d
php 08 <php.08@espeto.cec.ime.usp.br>
2018-01-23 22:28:11 -0200

    agencia com uso de métodos

--- Conta.php
+++ Conta.php
@@ -2,6 +2,8 @@
 class Conta{
     public $numero;
     public $saldo=0;
+    public $limite=100;
+    public $Agencia="agencia";
     function deposita(float $valor){
       if(is_float($valor)){
           $this->saldo +=$valor;
@@ -9,6 +11,26 @@ class Conta{
           die('O valor deve ser float');
       }
   }
+  function saca(float $valor){
+    if(is_float($valor)){
+        if ($this->saldo>=$valor){
+            $this->saldo -=$valor;
+        }else{
+            echo "Saldo : $this->saldo insuficiente".PHP_EOL;
+        }
+    }else{
+        die('O valor deve ser float');
+    }
+ }
+    function imprimeExtrato(){
+      echo "\tNumero: $this->numero".PHP_EOL;
+      echo "\tSaldo: $this->saldo".PHP_EOL;
+      echo "\tLimite: $this->limite".PHP_EOL;
+      echo "\tAgencia: $this->Agencia".PHP_EOL;
+    }
+    function consultaSaldo(){
+      echo "Saldo: $this->saldo".PHP_EOL;
+    }
 }
 
 ?>
--- TestaConta.php
+++ TestaConta.php
@@ -4,8 +4,16 @@ require 'Conta.php';
 
 $conta = new Conta();
 //
+$conta->numero=500;
 echo "\n\tSaldo Ini: $conta->saldo". PHP_EOL;
+
 $conta->deposita(2000);
 echo "\n\tSaldo: $conta->saldo".PHP_EOL;
+
+//
+$conta->saca(500);
+$conta->consultaSaldo();
+$conta->imprimeExtrato();
+
 
 ?>
